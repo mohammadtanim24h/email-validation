1a70227b9cf49aa71b24e04f390bb3a96cbf27d8
Mohammad Tanim <mohammad.tanim24h@gmail.com>
2022-03-09 12:59:56 +0600

    simple validation added

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
-console.log('Testing')
+const result = document.getElementById('validation-result');
+const validateEmail = () => {
+    const emailInput = document.getElementById('email-input');
+    const email = (emailInput.value).toLowerCase();
+    if(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email) === true) {
+        result.innerText = 'Your Email is Valid';
+        result.style.color = 'limegreen';
+    }
+    else {
+        result.innerText = 'Please Enter a Valid Email Address';
+        result.style.color = 'red';
+    }
+    emailInput.value = '';
+};
